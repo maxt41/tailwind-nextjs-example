03fa3edb95411616e3da8a6a225f6e381cf9106a
max <maxhthomas@outlook.com>
2023-10-27 19:04:04 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-center space-y-2">
+        <h1 className="text-xl">Tailwind CSS + NextJS</h1>
+        <button className="bg-blue-500 rounded p-3 text-white font-bold">Styling has never been so easy</button>
+      </div>
+    </>
+  )
+}
